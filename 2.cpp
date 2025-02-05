c478e11e8f9c11a591d2f66d228661327a66581e
SanaPerween07 <sanahmad15046@gmail.com>
2025-02-05 14:39:12 +0530

    Saved local changes before pull

---
+++ 2.cpp
@@ -0,0 +1,5 @@
+git checkout -- <filename>
+
+// You modified a file (file.txt) but haven’t staged or committed or added it yet. Now, you want to discard those changes and restore the last committed version.
+
+// Revert unstaged changes in a file.
